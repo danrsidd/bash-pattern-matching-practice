31f550f3f800ba94c0c1d237f7eca1a40e1f8650
Danyal <58652498+danrsidd@users.noreply.github.com>
2021-11-26 01:28:41 -0500

    Initial commit

---
+++ pattern_matching.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Pattern matching script for example server log file.
+# Input: Example server log file
+# Output: "Server Name: Error Code Number"
+
+# Step 1) Use grep searching command in conjunction with RegEx to extract the relvant information from the server log.
+
+# Step 2) Pipe the output of the grep search into the awk scripting command.
+
+# Step 3) To print our output in the specified output format, orgnaize desired fields with specified separators and redirect output into a new text file.
+
+grep -oP '(error\scode\s\d+)\D+(server\s\d+)' ./example_server_log.txt | awk '{print $7 " "$8": "$1,$2,$3}' > ./output.txt
